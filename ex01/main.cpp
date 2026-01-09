363d52a02ef23dca9c1d7c1e9404a15ad8e54b2d
Kimia <kimiaziari1997@gmail.com>
2026-01-09 15:00:39 +0100

    add more structure tests

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -1,18 +1,57 @@
+#include <iostream>
+#include <string>
 #include "iter.hpp"
 
-void printInt(int &x) {
-	std::cout << x << std::endl;
+// Non-const function (can modify elements)
+void incrementInt(int &x)
+{
+	x++;
 }
 
-void printString(const std::string &s) {
-	std::cout << s << std::endl;
+// Const functions (read-only)
+void printInt(const int &x)
+{
+	std::cout << x << " ";
 }
 
+void printString(const std::string &s)
+{
+	std::cout << s << " ";
+}
 
-int main( void ) {
-	int arr[] = {1, 2, 3};
-	const std::string sarr[] = {"hi", "bye"};
-	::iter(arr, 3, printInt);
-	::iter(sarr, 2, printString);
+int main()
+{
+	std::cout << "=== Test 1: int array (modifiable) ===" << std::endl;
+
+	int intArr[] = {1, 2, 3, 4};
+
+	iter(intArr, 4, incrementInt);
+	iter(intArr, 4, printInt);
+	std::cout << std::endl;
+
+	std::cout << "\n=== Test 2: const int array (read-only) ===" << std::endl;
+
+	const int constIntArr[] = {10, 20, 30};
+
+	iter(constIntArr, 3, printInt);
+	std::cout << std::endl;
+
+	// This should NOT compile
+	//iter(constIntArr, 3, incrementInt);
+
+	std::cout << "\n=== Test 3: string array ===" << std::endl;
+
+	std::string strArr[] = {"hello", "world"};
+
+	iter(strArr, 2, printString);
+	std::cout << std::endl;
+
+	std::cout << "\n=== Test 4: const string array ===" << std::endl;
+
+	const std::string constStrArr[] = {"bye", "world!"};
+
+	iter(constStrArr, 2, printString);
+	std::cout << std::endl;
+
 	return 0;
 }
